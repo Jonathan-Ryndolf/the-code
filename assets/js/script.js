e7ba569cab7ee265aeb684aea9bb4b9b8bd0c13f
Jonathan-Ryndolf <ryndolf2001@gmail.com>
2021-10-27 23:50:14 +0530

    rotate added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
-window.onscroll = function () {
-  var theta = (document.documentElement.scrollTop / 50) % Math.PI;
+(function () {
+  var throttle = function (type, name, obj) {
+    var obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+  throttle("scroll", "optimizedScroll");
+})();
 
-  document.getElementById("js-logo").style.transform =
-    "rotate(" + theta + "rad)";
-};
+var tl = document.getElementById("tl"),
+  tr = document.getElementById("tr"),
+  bl = document.getElementById("bl"),
+  br = document.getElementById("br");
+
+// to use the script *without* anti-jank, set the event to "scroll" and remove the anonymous function.
+
+window.addEventListener("optimizedScroll", function () {
+  tl.style.transform = "rotate(" + window.pageYOffset + "deg)";
+  tr.style.transform = "rotate(" + window.pageYOffset + "deg)";
+  bl.style.transform = "rotate(" + window.pageYOffset + "deg)";
+  br.style.transform = "rotate(-" + window.pageYOffset + "deg)";
+});
