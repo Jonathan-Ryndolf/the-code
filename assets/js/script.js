a7d3e5b6ed92ae00e0150441bc03e005be261449
Jonathan-Ryndolf <ryndolf2001@gmail.com>
2021-11-01 18:39:31 +0530

    dots added

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,29 @@ window.addEventListener("optimizedScroll", function () {
   bl.style.transform = "rotate(" + window.pageYOffset + "deg)";
   br.style.transform = "rotate(" + window.pageYOffset + "deg)";
 });
+
+$(document).ready(function () {
+  function changeDot() {
+    var scrollTop = $("html, body").scrollTop(),
+      section = $(".main-sec");
+
+    section.each(function (i, elem) {
+      var that = $(elem),
+        sectionScroll = that.offset().top,
+        sectionHeigth = that.outerHeight(),
+        distance = sectionScroll - scrollTop,
+        procent = (-distance * 100) / sectionHeigth;
+
+      if (procent >= -10 && procent <= 90) {
+        var dots = $(".dot");
+        var dotsEq = dots.eq(i);
+
+        dotsEq.prev().removeClass("active");
+        dotsEq.next().removeClass("active");
+        dotsEq.addClass("active");
+      }
+    }); //.each
+  } //.fn
+
+  $(document).on("scroll", changeDot);
+});
